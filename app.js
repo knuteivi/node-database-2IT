00208139820ced4a2aefed7d938a8ac38d556f3c
knuteivi <knuteivi@gmail.com>
2024-01-11 21:19:44 +0100

    Add render with parameters
    Add query to select rows from table Kunder and pass them to kunder.ejs

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 const express = require('express');
 const app = express();
-
+const PORT = 3000;
 const dbConn = require('./lib/db');
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res, next){
+    dbConn.query('SELECT * FROM kunde', function(err, rows){
+        // res.render('kunder', {data:rows});
+        console.log(rows);
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('kunder', {data:rows});
+        }
+    });
+});
+
+app.listen(PORT, function (err){
+    if (err) console.log(err);
+    console.log('Server listening on PORT', PORT);
+});
