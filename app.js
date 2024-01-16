e4fbc06d5e72166e9fdbdb081cb65279965089fb
knuteivi <knuteivi@gmail.com>
2024-01-16 14:13:24 +0100

    Add function for handling form data

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const app = express();
 const PORT = 3000;
 const dbConn = require('./lib/db');
 
+//This one is necessary to read form data using req.body
+app.use(express.urlencoded({extended: false}));
+
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res, next){
     dbConn.query('SELECT * FROM kunde', function(err, rows){
-        // res.render('kunder', {data:rows});
         console.log(rows);
         if (err) {
             console.log(err);
@@ -20,6 +22,25 @@ app.get('/', function(req, res, next){
 app.get('/add', function(req, res, next){
     //render to add.ejs
     res.render('add',{Id: '', Navn: 'Knut', Epost: ''});
+});
+
+app.post('/add', function(req, res, next){
+    console.log(req.body);
+    let id1 = req.body.id;
+    let navn1 = req.body.navn;
+    let epost1 = req.body.epost;
+    let errors = false;
+
+    if(!errors){
+        let form_data = {
+            id: id1,
+            navn: navn1,
+            epost: epost1
+        }
+        dbConn.query('INSERT INTO kunde SET ?', form_data, function(err, result){
+            res.redirect('/');
+        })
+    }
 });
 
 app.listen(PORT, function (err){
